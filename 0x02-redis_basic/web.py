c4c8693eae1e9cdfcc6bd83ecddf8a766da3cc9c
Abbk1109 <abbk1983@gmail.com>
2023-07-19 20:15:17 +0000

    update for task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,35 +2,43 @@
 """
 Caching request module
 """
+from flask import Flask, request
 import redis
-import requests
-from functools import wraps
-from typing import Callable
 
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
-    """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
-    return wrapper
+app = Flask(__name__)
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+@app.route('/some_url')
+def count_url_access():
+    url = request.url
 
-@track_get_page
-def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
-    """
-    response = requests.get(url)
-    return response.text
+    # Check if the URL's count is already cached
+    cache_key = f"count:{url}"
+    count = redis_client.get(cache_key)
+
+    if count is not None:
+        # If count is cached, return it
+        count = int(count)
+    else:
+        # If count is not cached, get it from the data store and cache it
+        # Replace this with your own logic to get the count from the data store
+        count = get_count_from_data_store(url)
+        
+        # Cache the count with an expiration time of 10 seconds
+        redis_client.setex(cache_key, 10, count)
+
+    # Increment the count and update the cache
+    count += 1
+    redis_client.setex(cache_key, 10, count)
+
+     return f"This URL has been accessed {count} times."
+
+ def get_count_from_data_store(url):
+    # Replace this function with your own logic to get the count from the data store
+    # For example, you might query a database or fetch the count from another source
+    # For this example, let's assume we're just returning 0.
+    return 0
+
+if __name__ == '__main__':
+    app.run()
